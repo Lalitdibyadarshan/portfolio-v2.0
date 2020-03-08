b89868f102838893527395054df0b3db7623dde5
Lalit Kanta Dibyadashan <32188590+Lalitdibyadarshan@users.noreply.github.com>
2020-03-08 18:19:47 +0530

    introduction section added

---
+++ src/app/root/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { IParams } from 'angular-particle/lib';
+import { ParticleConfig } from './config/particles.config';
+import { ParticleEventModeEnum } from './enums/particleEventMode.enum';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+	myStyle: object = {};
+	myParams: IParams;
+	width: number = 100;
+	height: number = 100;
+
+	private _numberOfParticles = 50;
+	private _particleColor = '#ffffff';
+
+
+	ngOnInit() {
+		const particleConfig = new ParticleConfig(
+			this._numberOfParticles,
+			this._particleColor,
+			ParticleEventModeEnum.REPULSE_MODE
+		);
+		this.myStyle = particleConfig.getStyle();
+		this.myParams = particleConfig.getParams()
+	}
+}
---
+++ src/app/root/enums/particleEventMode.enum.ts
@@ -0,0 +1,5 @@
+export enum ParticleEventModeEnum {
+    GRAB_MODE = 'grab',
+    PUSH_MODE = 'push',
+    REPULSE_MODE = 'repulse'
+}
